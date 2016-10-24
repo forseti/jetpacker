f586bfc310661a68497efbdfd603dfad7e826ffa
Donny Wijaya <donny.wijaya@leotech.local>
2016-10-24 14:35:09 +0800

    Moved the project to ratpack

--- workspace/src/main/groovy/forseti/generator/GeneratorController.groovy
+++
@@ -1,7 +0,0 @@
-package forseti.generator
-
-/**
- * Created by donny.wijaya on 19/10/2016.
- */
-class GeneratorController {
-}
--- workspace/src/main/groovy/forseti/generator/data/PackData.groovy
+++
@@ -1,7 +0,0 @@
-package forseti.generator.data
-
-/**
- * Created by donny.wijaya on 19/10/2016.
- */
-class PackData {
-}
