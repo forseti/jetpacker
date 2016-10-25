97ed0d7daa71b110351b263cff4f02ce7ae7ac7a
ruach <slickwolf@gmail.com>
2016-10-26 00:08:44 +0800

    Added some snippets to JetpackerCache (and also added Category enum)

---
+++ workspace/src/main/groovy/jetpacker/cache/Category.groovy
@@ -0,0 +1,10 @@
+package jetpacker.cache
+
+/**
+ * Created by wolf on 25/10/16.
+ */
+enum Category {
+    Sdk,
+    Box,
+    Database
+}
--- workspace/src/main/groovy/jetpacker/cache/JetpackerCache.groovy
+++ workspace/src/main/groovy/jetpacker/cache/JetpackerCache.groovy
@@ -1,7 +1,36 @@
 package jetpacker.cache
 
+import javax.inject.Singleton
+
 /**
  * Created by donny.wijaya on 25/10/2016.
  */
+//@Singleton
 class JetpackerCache {
+//
+//    SDK
+//    {
+//        openjdk
+//        sdkman
+//
+//        nvm
+//        node
+//
+//    }
+//
+//
+//    JetpackerCache() {
+//        LoadingCache<Key, Graph> graphs = CacheBuilder.newBuilder()
+//                .maximumSize(1000)
+//                .expireAfterWrite(10, TimeUnit.MINUTES)
+//                .removalListener(MY_LISTENER)
+//                .build(
+//                new CacheLoader<Key, Graph>() {
+//                    public Graph load(Key key) throws AnyException {
+//                        return createExpensiveGraph(key);
+//                    }
+//                });
+//
+//
+//    }
 }
